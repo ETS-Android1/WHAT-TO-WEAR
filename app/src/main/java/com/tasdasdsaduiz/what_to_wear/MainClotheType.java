506937121a48ba738f841818a15cf7ca761c59a9
obergjoakim <joakim.oeberg@gmail.com>
2022-02-21 14:30:56 +0100

    Testing commiting
    Signed-off-by: obergjoakim <joakim.oeberg@gmail.com>

--- app/src/main/java/com/tasdasdsaduiz/what_to_wear/MainClotheType.java
+++ app/src/main/java/com/tasdasdsaduiz/what_to_wear/MainClotheType.java
@@ -26,6 +26,13 @@ public class MainClotheType {
         float [] wst1 = {0.10f,0.20f,0.12f};
         subweight[1] = wst1;
 
+          // add the data for 1
+          maintypes[1] = "T-shirt";
+          String [] st2 = {"Normal", "Long-Sleeve", "No-sleeves"};
+          subtypes[1] = st1;
+          float [] wst2 = {0.10f,0.20f,0.12f};
+          subweight[1] = wst1;
+
 
 
 
